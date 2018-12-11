c9d37c8bf20a1640f4b4e83c4f9d468b81b605ad
Justin Kook <justinkook@gmail.com>
2018-12-11 03:13:50 -0500

    added menu nav bar

---
+++ js/overlay.js
@@ -0,0 +1,7 @@
+const toggleOverlay = () => {
+    $('#overlay').toggle();
+    $('#overlay').toggleClass('animate-menu');
+    $('body').toggleClass('hidden-overflow');
+}
+
+$('#open-menu').on('click', toggleOverlay);
