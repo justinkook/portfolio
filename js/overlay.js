76bc64025cef085220a3d115c2483fb2226cdbd3
Justin Kook <justinkook@gmail.com>
2018-12-11 05:02:48 -0500

    changed overlay to half height

--- js/overlay.js
+++ js/overlay.js
@@ -1,7 +1,7 @@
 const toggleOverlay = () => {
     $('#overlay').toggle();
     $('#overlay').toggleClass('animate-menu');
-    $('body').toggleClass('hidden-overflow');
+    // $('body').toggleClass('hidden-overflow');
 }
 
 $('#open-menu').on('click', toggleOverlay);
